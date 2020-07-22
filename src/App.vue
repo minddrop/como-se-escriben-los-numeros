902644ff9cbc9a603d9b536f5380782763ac8617
somen0101 <so_men0101@outlook.jp>
2020-07-23 06:33:27 +0900

    add style

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,24 @@
   <div id="app">
     <div class="menu-list">
       <router-link to="/">
-      <p class="menu-list_inicio">
-        Inicio
-      </p>
+       <p class="menu-list_inicio">
+         Inicio
+       </p>
       </router-link>
       <router-link to="/game">
-      <p class="menu-list_fuego">
-        Fuego
-      </p>
+       <p class="menu-list_fuego">
+         Fuego
+       </p>
       </router-link>
       <router-link to="/search">
-      <p class="menu-list_buscar">
-        Buscar
-      </p>
+       <p class="menu-list_buscar">
+         Buscar
+       </p>
       </router-link>
     </div>
-    <div class="main-content">  
-      <router-view />
-    </div>  
+   <div class="main-content">  
+     <router-view />
+   </div>  
   </div>
 </template>
 <style lang="scss">
