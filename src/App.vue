bfd39dfd2c7e6cba9b2a24958e1c9beb3369d8c8
minddrop <ryooooooo1@gmail.com>
2020-07-24 12:10:37 +0900

    formatted and made codes steady

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,35 @@
   <div id="app">
     <div class="menu-list">
       <router-link to="/">
-       <p class="menu-list_inicio">
-         Inicio
-       </p>
+        <p class="menu-list_inicio">
+          Inicio
+        </p>
       </router-link>
       <router-link to="/game">
-       <p class="menu-list_fuego">
-         Fuego
-       </p>
+        <p class="menu-list_fuego">
+          Fuego
+        </p>
       </router-link>
       <router-link to="/search">
-       <p class="menu-list_buscar">
-         Buscar
-       </p>
+        <p class="menu-list_buscar">
+          Buscar
+        </p>
       </router-link>
     </div>
-   <div class="main-content">  
-     <router-view />
-   </div>  
+    <div class="main-content">
+      <router-view />
+    </div>
   </div>
 </template>
 <style lang="scss">
- .menu-list {
-    display: flex;
-    &_inicio{
-      margin-left: 10px;
-      margin-right: 20px;
-    }
-    &_fuego{
-      margin-right: 20px;
-    }
- }
+.menu-list {
+  display: flex;
+  &_inicio {
+    margin-left: 10px;
+    margin-right: 20px;
+  }
+  &_fuego {
+    margin-right: 20px;
+  }
+}
 </style>
